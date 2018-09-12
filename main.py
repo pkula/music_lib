fcd67f9d8aec7eba603d6e7057ec71b0084e98ec
Lucy <px.kula@gmail.com>
2018-09-13 00:28:10 +0200

    eee

--- main.py
+++ main.py
@@ -5,30 +5,11 @@
 
 def main():
 
-    x = '1'
+    choice = '100'
     while x != '0':
         display_the_menu_options()
-        x = choose_option()
-        if x == '1':
-            fun_1():
-
-        elif x == '2':
-            fun_2():
-
-        elif x == '3':
-            fun_3():    
-
-        elif x == '4':
-            fun_4():
-
-        elif x == '5':
-            fun_5():
-
-        elif x == '6':
-            fun_6():
-
-        elif x == '7':
-            fun_7():
+        choice = input('choice option') 
+        choose_option(choice)
 
 
 
---
+++ music_reports.py
@@ -0,0 +1,21 @@
+def choice_option(your_input):
+    if your_input == '1':
+        fun_1():
+
+    elif your_input == '2':
+        fun_2():
+
+    elif your_input == '3':
+        fun_3():    
+
+    elif your_input == '4':
+        fun_4():
+
+    elif your_input == '5':
+        fun_5():
+
+    elif your_input == '6':
+        fun_6():
+
+    elif your_input == '7':
+        fun_7():
