cc4fd6fa955eb5c912a04694f5c855c554bdbaf5
Lucy <px.kula@gmail.com>
2018-09-12 13:16:06 +0200

    finish file.read

--- file_handler.py
+++ file_handler.py
@@ -1,8 +1,12 @@
 def file_read(filename = 'text_albums_data.txt'):
-    with open(filename, 'r') as file:
+    with open(filename , 'r') as file:
         list_of_lines = file.readlines()
-        print(list_of_lines)
-    return 0
+        new_list_of_lines = []
+        for list in list_of_lines:
+            list = list[0:-1]
+            list = list.split(',')
+            new_list_of_lines = new_list_of_lines + [list]            
+    return new_list_of_lines
 
 
-file_read()
+
