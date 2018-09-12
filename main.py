bd150e1bb5dfc6ac4f32aadab0a6fd2a43a3533d
Lucy <px.kula@gmail.com>
2018-09-12 12:13:51 +0200

    pierszy zarys main

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-import string
+# zaimportowac wszystkie pliki
+# zmienic nazwy funkcji pod if
+
+
+
+def main():
+
+    x = '1'
+    while x != '0':
+        display_the_menu_options()
+        x = choose_option()
+        if x == '1':
+            fun_1():
+
+        elif x == '2':
+            fun_2():
+
+        elif x == '3':
+            fun_3():    
+
+        elif x == '4':
+            fun_4():
+
+        elif x == '5':
+            fun_5():
+
+        elif x == '6':
+            fun_6():
+
+        elif x == '7':
+            fun_7():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
